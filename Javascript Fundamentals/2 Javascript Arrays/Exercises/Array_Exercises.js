28d7fb64e185caa4e77f32cb5b418b4cd50ad70a
artuKam <ksk.arthur@gmail.com>
2017-04-12 21:58:10 +0800

    Completed a few sections of Javascript Fundamentals

---
+++ Javascript Fundamentals/2 Javascript Arrays/Exercises/Array_Exercises.js
@@ -0,0 +1,56 @@
+var people = ["Greg", "Mary", "Devon", "James"]
+
+// Q1
+
+people.every(function(value) {
+	return console.log(value);
+})
+
+//Q2
+
+people.shift();
+
+//Q3
+
+people.pop();
+
+//Q4
+
+people.unshift("Matt");
+
+//Q5
+
+people.push("Arthur");
+
+//Q6
+
+for (var i = 0; i < people.length; i++) {
+	if (people[i] === "Mary") {
+		console.log(people[i]);
+		break;
+	} else {
+		console.log(people[i]);
+	}
+}
+
+//Q7
+
+var newPeople = people.slice(2);
+
+//Q8
+
+people.indexOf("Mary");
+
+//Q9
+
+people.indexOf("Foo");
+
+//Q10
+
+var people = ["Greg", "Mary", "Devon", "James"];
+
+people.splice(2, 1, "Elizabeth", "Artie");
+
+//Q11
+
+var withBob = people.concat("Bob");
