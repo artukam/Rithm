88e980f7d90bf12b99ef112ce62b188c2775d5b1
artukam <ksk.arthur@gmail.com>
2017-04-21 06:08:06 -0700

    Created sort functions for tables

--- Intermediate Javascript II/2 Intermediate jQuery/Exercises/Part 2 - Movie List/moviescript.js
+++ Intermediate Javascript II/2 Intermediate jQuery/Exercises/Part 2 - Movie List/moviescript.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
 	var $form = $("form");
 	var $table = $("table");
+	var storage = [];
 	
 	//Create new form item
 	$form.on("submit", function(event) {
 		event.preventDefault();
+		createRow($("#movie-title").val(), $("#movie-rating").val());
+	})
+
+	//Create row function
+	function createRow(title, rating) {
 		var newRow = $("<tr>");
 		var newMovie = $("<td>");
 		var newRating = $("<td>");
@@ -12,8 +18,8 @@ $(document).ready(function() {
 		var newButton = $("<button>")
 		newRow.addClass("table-text");
 		newMovie.addClass("col-md-6");
-		newMovie.text($("#movie-title").val());
-		newRating.text($("#movie-rating").val());
+		newMovie.text(title);
+		newRating.text(rating);
 		newButton.addClass("btn btn-danger");
 		newButton.attr("type", "button");
 		newButton.text("Delete");
@@ -26,5 +32,62 @@ $(document).ready(function() {
 			event.preventDefault();
 			newRow.remove();
 		})
-	})
+		storage.push({
+			movieTitle: title,
+			movieRating: rating
+		});
+	}
+	//Sort movies by ascending order
+
+
+	//Sort objects in array by titles
+	function sortTitlesArray(array, direction) {
+		var valuesArray = [];
+		var objectsArray = [];
+		for (var i = 0; i < array.length; i++) {
+			valuesArray.push(array[i]["movieTitle"]);
+		}
+		if (direction === "ascending") {
+			valuesArray.sort();
+		} else if (direction === "descending") {
+			valuesArray.sort().reverse();
+		}
+		for (var j = 0; j < valuesArray.length; j++) {
+			for (var k = 0; k < array.length; k++) {
+				if (valuesArray[j] === array[k]["movieTitle"]) {
+					objectsArray.push(array[k]);
+				}
+			}
+		}
+		return objectsArray;
+	}
+
+	//Sort objects in array by ratings
+	function sortRatingsArray(array, direction) {
+		var valuesArray = [];
+		var objectsArray = [];
+		for (var i = 0; i < array.length; i++) {
+			valuesArray.push(array[i]["movieRating"]);
+		}
+		if (direction === "ascending") {
+			valuesArray.sort(function(a,b) {return a - b});
+		} else if (direction === "descending") {
+			valuesArray.sort(function(a,b) {return b - a});
+		}
+		for (var j = 0; j < valuesArray.length; j++) {
+			for (var k = 0; k < array.length; k++) {
+				if (valuesArray[j] === array[k]["movieRating"]) {
+					objectsArray.push(array[k]);
+				}
+			}
+		}
+		return objectsArray;
+	}
+
+	//Create rows from arrays with objects
+	function createRowsFromArray(array) {
+		for (var i = 0; i < array.length; i++) {
+			createRow(array[i]["movieTitle"], array[i]["movieRating"]);
+		}
+	}
 })
