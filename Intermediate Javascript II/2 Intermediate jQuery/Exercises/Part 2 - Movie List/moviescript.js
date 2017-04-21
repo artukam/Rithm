43092ea6d875d540252e4e180fd95ec9dae090cc
artukam <ksk.arthur@gmail.com>
2017-04-21 11:04:19 -0700

    Completed Movie List exercise (including localStorage)

--- Intermediate Javascript II/2 Intermediate jQuery/Exercises/Part 2 - Movie List/moviescript.js
+++ Intermediate Javascript II/2 Intermediate jQuery/Exercises/Part 2 - Movie List/moviescript.js
@@ -49,6 +49,40 @@ $(document).ready(function() {
 		localStorage.setItem("MovieList", JSON.stringify(storage));
 		$form.trigger("reset");
 	}
+	//Create row function for sort
+	function createRowSort(title, rating) {
+		var newRow = $("<tr>");
+		var newMovie = $("<td>");
+		var newRating = $("<td>");
+		var newDelete = $("<td>");
+		var newButton = $("<button>")
+		newRow.addClass("table-text");
+		newMovie.addClass("col-md-6");
+		newMovie.text(title);
+		newRating.text(rating);
+		newButton.addClass("btn btn-danger");
+		newButton.attr("type", "button");
+		newButton.text("Delete");
+		$tableBody.append(newRow);
+		newRow.append(newMovie);
+		newRow.append(newRating);
+		newRow.append(newDelete);
+		newDelete.append(newButton);
+		newButton.on("click", function(event){
+			event.preventDefault();
+			if (storage.length > 0) {
+				for (var i = 0; i < storage.length; i++) {
+					if (storage[i]["movieTitle"] === newMovie.text()) {
+						storage.splice(i,1);
+						break;
+					}
+				}
+			}
+			localStorage.setItem("MovieList", JSON.stringify(storage))
+			newRow.remove();
+		})
+	}
+
 	//Sort movies by ascending order (title)
 	$("#sort-icon-up-letter").on("click", function(event) {
 		if (storage.length > 0) {
@@ -58,6 +92,37 @@ $(document).ready(function() {
 			localStorage.setItem("MovieList", JSON.stringify(storage));
 		}
 	})
+
+	//Sort movies by descending order (title)
+	$("#sort-icon-down-letter").on("click", function(event) {
+		if (storage.length > 0) {
+			storage = sortTitlesArray(storage, "descending");
+			$tableBody.empty();
+			createRowsFromArray(storage);
+			localStorage.setItem("MovieList", JSON.stringify(storage));
+		}
+	})
+
+	//Sort movies by ascending order (rating)
+	$("#sort-icon-up-number").on("click", function(event) {
+		if (storage.length > 0) {
+			storage = sortRatingsArray(storage, "ascending");
+			$tableBody.empty();
+			createRowsFromArray(storage);
+			localStorage.setItem("MovieList", JSON.stringify(storage));
+		}
+	})
+
+	//Sort movies by descending order (rating)
+	$("#sort-icon-down-number").on("click", function(event) {
+		if (storage.length > 0) {
+			storage = sortRatingsArray(storage, "descending");
+			$tableBody.empty();
+			createRowsFromArray(storage);
+			localStorage.setItem("MovieList", JSON.stringify(storage));
+		}
+	})
+
 
 	//Sort objects in array by titles
 	function sortTitlesArray(array, direction) {
@@ -75,6 +140,7 @@ $(document).ready(function() {
 			for (var k = 0; k < array.length; k++) {
 				if (valuesArray[j] === array[k]["movieTitle"]) {
 					objectsArray.push(array[k]);
+					break;
 				}
 			}
 		}
@@ -83,31 +149,44 @@ $(document).ready(function() {
 
 	//Sort objects in array by ratings
 	function sortRatingsArray(array, direction) {
-		var valuesArray = [];
-		var objectsArray = [];
-		for (var i = 0; i < array.length; i++) {
-			valuesArray.push(array[i]["movieRating"]);
-		}
 		if (direction === "ascending") {
-			valuesArray.sort(function(a,b) {return a - b});
-		} else if (direction === "descending") {
-			valuesArray.sort(function(a,b) {return b - a});
-		}
-		for (var j = 0; j < valuesArray.length; j++) {
-			for (var k = 0; k < array.length; k++) {
-				if (valuesArray[j] === array[k]["movieRating"]) {
-					objectsArray.push(array[k]);
+			array.sort(function(a,b) {
+				if (a["movieRating"] < b["movieRating"]) {
+					return -1;
+				} else if (a["movieRating"] > b["movieRating"]) {
+					return 1;
+				} else {
+					return 0;
 				}
-			}
+			});
+		} else if (direction === "descending") {
+			array.sort(function(a,b) {
+				if (b["movieRating"] < a["movieRating"]) {
+					return -1;
+				} else if (b["movieRating"] > a["movieRating"]) {
+					return 1;
+				} else {
+					return 0;
+				}
+			});
 		}
-		return objectsArray;
+		return array;
 	}
 
 	//Create rows from arrays with objects
 	function createRowsFromArray(array) {
 		var dummyArray = array.concat();
 		for (var i = 0; i < dummyArray.length; i++) {
-			createRow(dummyArray[i]["movieTitle"], dummyArray[i]["movieRating"]);
+			createRowSort(dummyArray[i]["movieTitle"], dummyArray[i]["movieRating"]);
 		}
+	}
+	//Import local storage
+	var retrieveStorage = JSON.parse(localStorage.getItem("MovieList"));
+	if (Array.isArray(retrieveStorage)) {
+		storage = retrieveStorage;
+	}
+	console.log(storage);
+	if (storage.length > 0) {
+		createRowsFromArray(storage);
 	}
 })
