b2072594e2999a37b670e579c73fb4a4a0c135b8
artukam <ksk.arthur@gmail.com>
2017-05-08 15:42:19 -0700

    Completed module exercises

---
+++ Python_Intro_I/module.py
@@ -0,0 +1,29 @@
+"""
+Q1
+A module is a library of functions and code which you can import into a file to be used.
+
+Q2
+Using import 'library' importing each function through from 'library' import 'function' or importing everything through from 'library' import *.
+
+Q3
+The purpose of importing is to bring in the functions that you want to use in your code
+
+Q4
+- to generate a random number to trigger an event
+- to choose a random value from a provided list
+- to generate a random numbers as part of a series of tests to test a function.
+
+Q5
+ImportError occurs when you try to import a function that does not exist in a library.
+
+Q6
+Ordereddict would be useful if you needed to keep track on the order of when certain items were entered into a dict.
+
+Q7
+Defaultdict is useful where you need to check whether certain keys exist in a dict as it returns None.
+
+Q8
+This code checks to see if the file is being directly run or being imported.  If it is directly run it will import the 
+text or functions at 'pass'
+
+"""
